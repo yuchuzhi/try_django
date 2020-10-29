0020b4cdb12dce5f17a322094789995750150f47
Chuzhi Yu <chuzhi.yu_1@nxp.com>
2020-10-29 18:45:14 +0800

    update html

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from .models import Article, Category, Banner, Tag
-
+from .models import Article, Category, Banner, Tag, Link
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def home(request):
@@ -22,12 +22,20 @@ def index(request):
     banner = Banner.objects.filter(is_active=True)[0:4]
     tui = Article.objects.filter(tui__id=1)[:3]
     allarticle = Article.objects.all().order_by('-id')
+    hot = Article.objects.all().order_by('views')[:3]
+    remen = Article.objects.filter(tui__id=2)[:3]
+    tags = Tag.objects.all()
+    link = Link.objects.all()
 
     context = {
         "title": "Z-Blog",
         "categorys": categorys,
         "banner": banner,
         "tui": tui,
+        "hot": hot,
+        "remen": remen,
+        "tags": tags,
+        'link':link,
         "allarticle": allarticle,
     }
 
@@ -40,19 +48,61 @@ def list(request, lid):
     remen = Article.objects.filter(tui__id=2)[:2]#右侧的热门推荐
     allcategory = Category.objects.all()#导航所有分类
     tags = Tag.objects.all()#右侧所有文章标签
+
+    page = request.GET.get('page')#在URL中获取当前页面数
+    paginator = Paginator(list, 5)#对查询到的数据对象list进行分页，设置超过5条数据就分页
+    try:
+        list = paginator.page(page)#获取当前页码的记录
+    except PageNotAnInteger:
+        list = paginator.page(1)#如果用户输入的页码不是整数时,显示第1页的内容
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)#如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
     return render(request, 'list.html', locals())
 
-def show(request,sid):
-    pass
 
+def show(request,sid):
+    show = Article.objects.get(id=sid)#查询指定ID的文章
+    allcategory = Category.objects.all()#导航上的分类
+    tags = Tag.objects.all()#右侧所有标签
+    remen = Article.objects.filter(tui__id=2)[:6]#右侧热门推荐
+    hot = Article.objects.all().order_by('?')[:10]#内容下面的您可能感兴趣的文章，随机推荐
+    previous_blog = Article.objects.filter(created_time__gt=show.created_time,category=show.category.id).first()
+    next_blog = Article.objects.filter(created_time__lt=show.created_time,category=show.category.id).last()
+    show.views = show.views + 1
+    show.save()
+    return render(request, 'show.html', locals())
 
 def tag(request, tag):
-    pass
-
+    list = Article.objects.filter(tags__name=tag)#通过文章标签进行查询文章
+    remen = Article.objects.filter(tui__id=2)[:6]
+    allcategory = Category.objects.all()
+    tname = Tag.objects.get(name=tag)#获取当前搜索的标签名
+    page = request.GET.get('page')
+    tags = Tag.objects.all()
+    paginator = Paginator(list, 5)
+    try:
+        list = paginator.page(page)  # 获取当前页码的记录
+    except PageNotAnInteger:
+        list = paginator.page(1)  # 如果用户输入的页码不是整数时,显示第1页的内容
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages)  # 如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
+    return render(request, 'tags.html', locals())
 
 def search(request):
-    pass
-
+    ss=request.GET.get('search')#获取搜索的关键词
+    list = Article.objects.filter(title__icontains=ss)#获取到搜索关键词通过标题进行匹配
+    remen = Article.objects.filter(tui__id=2)[:6]
+    allcategory = Category.objects.all()
+    page = request.GET.get('page')
+    tags = Tag.objects.all()
+    paginator = Paginator(list, 10)
+    try:
+        list = paginator.page(page) # 获取当前页码的记录
+    except PageNotAnInteger:
+        list = paginator.page(1) # 如果用户输入的页码不是整数时,显示第1页的内容
+    except EmptyPage:
+        list = paginator.page(paginator.num_pages) # 如果用户输入的页数不在系统的页码列表中时,显示最后一页的内容
+    return render(request, 'search.html', locals())
 
 def about(request):
     pass
